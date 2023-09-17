254c566888e846618c9f865d92a80157588f915a
KlugSolutions <tomasz.kluge@gmail.com>
2023-09-17 21:17:59 +0200

    def write initialize

--- main.py
+++ main.py
@@ -5,3 +5,16 @@ pygame.init()
 width = 700
 height = 700
 screen = pygame.display.set_mode((width, height))
+
+
+def write(text, x, y, size, color):
+    font = pygame.font.SysFont('serif', size)
+    text = font.render(text, 1, (0, 0, 255))
+    x = (width - text.get_width()) / 2
+    y = (height - text.get_height()) / 2
+    screen.blit(text, (x, y))
+
+
+write('Press space to start', 80, 150, 20)
+
+pygame.display.update()
