96f477c2a2fba890147047c1ca62b12f694d5e3a
tomaszkluge <tomasz.kluge@gmai.com>
2023-09-18 05:19:14 -0400

    added diver and collision def

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import random
 
 pygame.init()
 
-width = 700
+width = 1000
 height = 500
 screen = pygame.display.set_mode((width, height))
 
@@ -32,25 +32,41 @@ class Rock():
 
     def move(self, speed):
         self.x -= speed
+        self.shape_up = pygame.Rect(self.x, self.y_up, self.width, self.height_up)
+        self.shape_down = pygame.Rect(self.x, self.y_down, self.width, self.height_down)
+
 
     def draw(self):
-        pygame.draw.rect(screen, self.color, (self.x, self.y_up, self.width, self.height_up), 0)
-        pygame.draw.rect(screen, self.color, (self.x, self.y_down, self.width, self.height_down))
+        pygame.draw.rect(screen, self.color, self.shape_up, 0)
+        pygame.draw.rect(screen, self.color, self.shape_down, 0)
+
+
+    def collision(self, player):
+        if self.shape_up.colliderect(player) or self.shape_down.colliderect(player):
+            return True
+        else:
+            return False
 
 class Diver():
 
         def __init__(self, x, y):
             self.x = x
             self.y = y
-            self.height = 20
-            self.width = 20
+            self.height = 100
+            self.width = 100
             self.shape = pygame.Rect(self.x, self.y, self.width, self.height)
             self.graphic = pygame.image.load(os.path.join('cave_diver.png'))
+        def draw(self):
+            screen.blit(self.graphic, (self.x, self.y))
+        def move(self, speed):
+            self.y = self.y + speed
 
 
 Rocks = []
 for i in range(21):
     Rocks.append(Rock(i * width / 20, width / 20))
+player = Diver(250, 275)
+dy = 0
 
 running = True
 while running:
@@ -60,6 +76,13 @@ while running:
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_SPACE and view == 'menu':
                 view = 'game'
+            if event.key == pygame.K_UP:
+                dy = -1
+            if event.key == pygame.K_DOWN:
+                dy = 1
+        elif event.type == pygame.KEYUP:
+            if event.key == pygame.K_UP or event.key == pygame.K_DOWN:
+                dy = 0
 
     screen.fill((0, 0, 0))
 
@@ -71,11 +94,19 @@ while running:
         for p in Rocks:
             p.move(1)
             p.draw()
+            if p.collision(player.shape):
+                view = "end"
         for p in Rocks:
             if p.x <= -p.width:
                 Rocks.remove(p)
                 Rocks.append(Rock(width, width / 20))
-
+        player.draw()
+        player.move(dy)
+    elif view == "end":
+        graphic = pygame.image.load(os.path.join('cave_diver.png'))
+        screen.blit(graphic, (80,30))
+        write("You loose", 50, 290, 20, color=(255, 0, 0))
+        write('Press space to start', 80, 150, 20, color=(0, 0, 255))
     pygame.display.update()
 
 pygame.quit()
